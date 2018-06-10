cba0b2a77a19ec28c8bfba95385802ce53dcce6e
Josh Young <j22cal@gmail.com>
2018-06-10 16:33:20 -0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root    'notes#index'
+  get     'notes'           => 'notes#index', as: 'notes'
+  post    'notes'           => 'notes#create'
+  get     'notes/new'       => 'notes#new',   as: 'new_note'
+  get     'notes/:id/edit'  => 'notes#edit',  as: 'edit_note'
+  get     'notes/:id'       => 'notes#show',  as: 'note'
+  patch   'notes/:id'       => 'notes#update'
+  put     'notes/:id'       => 'notes#update'
+  delete  'notes/:id'       => 'notes#destroy'
 end
