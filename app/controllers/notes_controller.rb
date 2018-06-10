01ea03a86ab5699fc8f6b9a0c76d42257675fe05
Josh Young <j22cal@gmail.com>
2018-06-10 16:33:54 -0500

    Add controller methods

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,37 @@
 class NotesController < ApplicationController
+  def index
+    @notes = Note.all
+  end
+
+  def create
+    @note = Note.new(subject: params[:note][:subject], body: params[:note][:body])
+    if @note.save
+      redirect_to notes_url
+    else
+      render :new
+    end
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def edit
+    @note = Note.find_by(id: params[:id])
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    note = Note.find(params[:id])
+    note.update(subject: params[:note][:subject], body: params[:note][:body])
+    redirect_to(notes_url)
+  end
+
+  def destroy
+    Note.find(params[:id]).destroy
+    redirect_to(notes_url)
+  end
 end
