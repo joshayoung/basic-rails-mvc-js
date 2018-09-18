5ab4e05a6b67bff877aa46d62e4ca86529d9e635
Josh Young <j22cal@gmail.com>
2018-09-18 17:53:19 -0500

    Correct and run migration

--- db/migrate/20180610213056_create_notes.rb
+++ db/migrate/20180610213056_create_notes.rb
@@ -1,7 +1,7 @@
 class CreateNotes < ActiveRecord::Migration[5.0]
   def change
     create_table :notes do |t|
-      t.string, :subject
+      t.string :subject
       t.string :body
 
       t.timestamps
